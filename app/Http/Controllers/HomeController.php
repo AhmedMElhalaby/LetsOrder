409124814d89da3edd87b82947e2a522e47317b1
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-03-10 16:31:06 +0200

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Constant;
+use App\Helpers\Functions;
 use App\Http\Requests\VerifyPaymentRequest;
+use App\Models\Transaction;
 use App\Models\User;
 use App\Models\VerifyAccounts;
 use Illuminate\Http\Request;
@@ -17,8 +20,20 @@ class HomeController extends Controller
         return view('privacy');
     }
 
-    public function verify_payment(VerifyPaymentRequest $request){
-        return $request->persist();
+    public function verify_payment(Request $request){
+        $status = false;
+        if ($request->filled('tap_id')) {
+            $Object = (new Transaction)->where('payment_token',$request->tap_id)->first();
+            if($Object){
+                $Response = Functions::CheckPayment(Constant::PAYMENT_METHOD['Tap'],$Object);
+                if($Response){
+                    $status = true;
+                    $Object->setStatus(Constant::TRANSACTION_STATUS['Paid']);
+                    $Object->save();
+                }
+            }
+        }
+        return view('verify_payment',compact('status'));
     }
     public function verify(Request $request){
         if($request->has('token')){
